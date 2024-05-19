12b1eed1d67325987c51743e6ea54da4b413bb7c
Michael Ferreira <michaeljf.2007@gmail.com>
2024-05-18 23:27:29 -0400

    Blog Title

---
+++ src/Components/Blog-Title.jsx
@@ -0,0 +1,8 @@
+import "./Blog-Title.css";
+
+function Blog_Title() {
+    const neighbourhood = "Erbsville";
+    return <h1 id="neighbourhood-title">{neighbourhood} Today</h1>;
+}
+
+export default Blog_Title;
